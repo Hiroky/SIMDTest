c39f09388d8f25a6f5cc20a99ba35af10d57b52f
hiroky <gan.morishita@gmail.com>
2017-01-09 18:01:36 +0900

    dot等を追加

--- SIMDTest/SIMDMath.h
+++ SIMDTest/SIMDMath.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <xmmintrin.h>
+#include <immintrin.h>
 
 typedef __m128 SIMDVector;
 #define FORCEINLINE __forceinline
@@ -8,41 +8,6 @@ typedef __m128 SIMDVector;
 namespace SIMD {
 
 	#define SIMD_SHUFFLEMASK(A0,A1,B2,B3) ((A0) | ((A1)<<2) | ((B2)<<4) | ((B3)<<6))
-
-	FORCEINLINE SIMDVector MakeVector(float x, float y, float z, float w)
-	{
-		return _mm_setr_ps(x, y, z, w);
-	}
-
-	FORCEINLINE SIMDVector LoadVector(const float* ptr)
-	{
-		return _mm_loadu_ps(ptr);
-	}
-
-	FORCEINLINE SIMDVector VectorMultiply(SIMDVector a, SIMDVector b)
-	{
-		return _mm_mul_ps(a, b);
-	}
-
-	FORCEINLINE SIMDVector VectorAdd(SIMDVector a, SIMDVector b)
-	{
-		return _mm_add_ps(a, b);
-	}
-
-	FORCEINLINE SIMDVector VectorSub(SIMDVector a, SIMDVector b)
-	{
-		return _mm_sub_ps(a, b);
-	}
-
-	FORCEINLINE SIMDVector VectorMax(SIMDVector a, SIMDVector b)
-	{
-		return _mm_max_ps(a, b);
-	}
-
-	FORCEINLINE SIMDVector VectorMin(SIMDVector a, SIMDVector b)
-	{
-		return _mm_min_ps(a, b);
-	}
 
 	template <int index>
 	FORCEINLINE SIMDVector VectorReplicate(SIMDVector a)
@@ -61,18 +26,124 @@ namespace SIMD {
 	{
 		return _mm_shuffle_ps(a, b, SIMD_SHUFFLEMASK(x, y, z, w));
 	}
+
+	FORCEINLINE SIMDVector MakeVector(float x, float y, float z, float w)
+	{
+		return _mm_setr_ps(x, y, z, w);
+	}
+
+	FORCEINLINE SIMDVector LoadVector(const float* ptr)
+	{
+		return _mm_loadu_ps(ptr);
+	}
+
+	FORCEINLINE SIMDVector LoadVectorAligned(const float* ptr)
+	{
+		return _mm_load_ps(ptr);
+	}
+
+	FORCEINLINE SIMDVector LoadVectorFloat3(const float* ptr, float w = 0)
+	{
+		return MakeVector(ptr[0], ptr[1], ptr[2], w);
+	}
+
+	FORCEINLINE void StoreVector(float* ptr, SIMDVector v)
+	{
+		return _mm_storeu_ps(ptr, v);
+	}
+
+	FORCEINLINE void StoreVectorFloat1(float* ptr, SIMDVector v)
+	{
+		return _mm_store_ss(ptr, v);
+	}
+
+	FORCEINLINE void StoreVectorAligned(float* ptr, SIMDVector v)
+	{
+		return _mm_store_ps(ptr, v);
+	}
+
+	FORCEINLINE SIMDVector VectorAdd(SIMDVector a, SIMDVector b)
+	{
+		return _mm_add_ps(a, b);
+	}
+
+	FORCEINLINE SIMDVector VectorSub(SIMDVector a, SIMDVector b)
+	{
+		return _mm_sub_ps(a, b);
+	}
+
+	FORCEINLINE SIMDVector VectorMultiply(SIMDVector a, SIMDVector b)
+	{
+		return _mm_mul_ps(a, b);
+	}
+
+	FORCEINLINE SIMDVector VectorDivide(SIMDVector a, SIMDVector b)
+	{
+		return _mm_div_ps(a, b);
+	}
+
+	// NOTE: 近似計算なので精度が低いことに注意
+	FORCEINLINE SIMDVector VectorRcp(SIMDVector a)
+	{
+		return _mm_rcp_ps(a);
+	}
+
+	FORCEINLINE SIMDVector VectorSqrt(SIMDVector a)
+	{
+		return _mm_sqrt_ps(a);
+	}
+
+	// NOTE: 近似計算なので精度が低いことに注意
+	FORCEINLINE SIMDVector VectorRsqrt(SIMDVector a)
+	{
+		return _mm_rsqrt_ps(a);
+	}
+
+	FORCEINLINE SIMDVector VectorMax(SIMDVector a, SIMDVector b)
+	{
+		return _mm_max_ps(a, b);
+	}
+
+	FORCEINLINE SIMDVector VectorMin(SIMDVector a, SIMDVector b)
+	{
+		return _mm_min_ps(a, b);
+	}
+
+	FORCEINLINE SIMDVector VectorDot(SIMDVector a, SIMDVector b)
+	{
+#if 1
+		// need SSE3
+		SIMDVector m = VectorMultiply(a, b);
+		m = _mm_hadd_ps(m, m);
+		m = _mm_hadd_ps(m, m);
+		return m;
+#else
+		SIMDVector m0, m1;
+		m0 = VectorMultiply(a, b);
+		m1 = VectorSwizzle<2, 3, 0, 1>(m0);
+		m0 = VectorAdd(m0, m1);
+		m1 = VectorSwizzle<1, 2, 3, 0>(m0);
+		return VectorAdd(m0, m1);
+#endif
+	}
+
+	FORCEINLINE SIMDVector VectorMultiplyAdd(SIMDVector a, SIMDVector b, SIMDVector c)
+	{
+		return _mm_add_ps(_mm_mul_ps(a, b), c);
+	}
+
 }
 
 
 /**
  * operator overrides
  */
-SIMDVector operator +(SIMDVector a, SIMDVector b)
+FORCEINLINE SIMDVector operator +(SIMDVector a, SIMDVector b)
 {
 	return SIMD::VectorAdd(a, b);
 }
 
-SIMDVector operator *(SIMDVector a, SIMDVector b)
+FORCEINLINE SIMDVector operator *(SIMDVector a, SIMDVector b)
 {
 	return SIMD::VectorMultiply(a, b);
 }
--- SIMDTest/SIMDTest.cpp
+++ SIMDTest/SIMDTest.cpp
@@ -34,6 +34,7 @@ public:
 	FORCEINLINE SIMDVector ToSIMD() const { return SIMD::LoadVector(m); }
 };
 
+typedef ALIGN(16) Vector4 AVector4;
 
 
 
@@ -53,12 +54,30 @@ struct Test
 
 int main()
 {
+	AVector4 a(1, 1, 1, 1);
+	auto& ra = (SIMDVector&)a;
+	//auto mulA = ra * ra;
+	auto mulA = _mm_mul_ps(ra, ra);
+
+	AVector4 b(1, 0, 1, 0);
+	//auto rb = b.ToSIMD();
+	auto rb = _mm_load_ps((float*)&b);
+	auto mulB = _mm_mul_ps(rb, rb);
+	//auto mulB = rb * rb;
+
+	auto dp = SIMD::VectorDot(ra, rb);
+
+#if 0
 	Vector4 a(1, 0.2f, 0.5f, 0.7f), b(2, 3, 5, 4);
 	auto ra = a.ToSIMD();
 	auto rb = b.ToSIMD();
 
-	auto rmax = SIMD::VectorMax(ra, rb);
+	AVector4 alignedVec(1, 1, 1, 1);
+
+	auto rmax = SIMD::VectorMax(ra, (SIMDVector&)alignedVec);
 	auto swizzle = SIMD::VectorSwizzle<1, 1, 2, 3>(ra);
+	auto mul = (SIMDVector&)alignedVec * (SIMDVector&)alignedVec;
+#endif
 
 #if 0
 	size_t size = sizeof(Test);
@@ -69,6 +88,10 @@ int main()
 		assert(((size_t)a & 0xff) == 0);
 	}
 #endif
+
+	SIMD::StoreVectorAligned((float*)&a, ra);
+	SIMD::StoreVectorAligned((float*)&b, rb);
+	printf("%f, %f\n", a.x, b.x);
 
     return 0;
 }
